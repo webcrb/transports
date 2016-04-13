a7d8c3e0e74ca2b8e27bc10bcdd9c14ece9c4ab8
webcrb <webcrb@gmail.com>
2016-04-13 10:52:46 +0200

    topbar & navbar

--- site/web/app/themes/crb/templates/header.php
+++ site/web/app/themes/crb/templates/header.php
@@ -1,11 +1,23 @@
 <header class="banner">
-  <div class="topbar text-xs-right">
+  <div class="topbar">
       <div class="container">
-        <ul class="nav nav-inline">
-          <li class="nav-item"><a href="#" class="nav-link">menuitem</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">menuitem</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">menuitem</a></li>
+        <ul class="nav nav-inline text-xs-right">
+          <li class="nav-item dropdown">
+             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Les autres sites</a>
+              <div class="dropdown-menu" aria-labelledby="sites">
+                <a class="dropdown-item" href="http://www.bretagne.bzh">bretagne.bzh</a>
+                <a class="dropdown-item" href="http://jeunes.bretagne.bzh">jeunes.bretagne.bzh</a>
+                <a class="dropdown-item" href="http://entreprise.bretagne.bzh">entreprise.bretagne.bzh</a>
+                <a class="dropdown-item" href="http://mer-littoral.bretagne.bzh">mer-littoral.bretagne.bzh</a>
+                <a class="dropdown-item" href="http://europe.bretagne.bzh">europe.bretagne.bzh</a>
+              </div>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="<?php echo wp_login_url(); ?>" title="Login"><i class="icon-sign-in"></i> Se connecter</a>
+          </li>
+
         </ul>
+        
       </div>
   </div>
     <nav class="navbar navbar-dark bg-inverse navbar-full">
@@ -14,15 +26,15 @@
           &#9776;
         </button>
         <div class="collapse navbar-toggleable-xs" id="navigation">
-          <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+          <a class="navbar-brand rb" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
           <?php
             if (has_nav_menu('primary_navigation')) :
               wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
             endif;
           ?>
           <form class="form-inline pull-xs-right">
-            <input class="form-control" type="text" placeholder="Search">
-            <button class="btn btn-primary-outline" type="submit">Search</button>
+            <input class="form-control" type="text" placeholder="Rechercher">
+            <button class="btn btn-secondary sr-only" type="submit">Rechercher</button>
           </form>
         </div>
       </div>
